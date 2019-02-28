4dda9754c1bb4de8f1578252b6a08f869cea2a92
knight <tocaldera6734@gmail.com>
2019-02-28 16:07:04 +0530

    Added explicit intent and bundle data

---
+++ app/src/main/java/com/xsample/lesson4a/Main2Activity.java
@@ -0,0 +1,37 @@
+package com.xsample.lesson4a;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        textView = findViewById(R.id.textView);
+        Bundle bundle = getIntent().getExtras();
+        String value = bundle.getString("key");
+        textView.setText(value);
+
+    }
+
+}
--- app/src/main/java/com/xsample/lesson4a/MainActivity.java
+++ app/src/main/java/com/xsample/lesson4a/MainActivity.java
@@ -1,13 +1,25 @@
 package com.xsample.lesson4a;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+    }
+
+    public void goToNextPage(View view) {
+        Intent intent = new Intent(this,Main2Activity.class);
+        intent.putExtra("key",editText.getText().toString());
+        startActivity(intent);
     }
 }
